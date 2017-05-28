c609bcd2509fdedcfad1dbaa23956ca7461026e7
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-05-28 23:45:35 +0200

    style: Gaussian Naive Bayes on Terrain Data

--- L2_Naive_Bayes/A_GaussianNB Deployment on Terrain Data/prep_terrain_data.py
+++ L2_Naive_Bayes/A_GaussianNB Deployment on Terrain Data/prep_terrain_data.py
@@ -3,8 +3,8 @@ import random
 
 
 def makeTerrainData(n_points=1000):
-    ###############################################################################
-    ### make the toy dataset
+
+    # make the toy dataset
     random.seed(42)
     grade = [random.random() for ii in range(0, n_points)]
     bumpy = [random.random() for ii in range(0, n_points)]
@@ -14,7 +14,7 @@ def makeTerrainData(n_points=1000):
         if grade[ii] > 0.8 or bumpy[ii] > 0.8:
             y[ii] = 1.0
 
-        ### split into train/test sets
+    # split into train/test sets
     X = [[gg, ss] for gg, ss in zip(grade, bumpy)]
     split = int(0.75 * n_points)
     X_train = X[0:split]
@@ -27,17 +27,13 @@ def makeTerrainData(n_points=1000):
     grade_bkg = [X_train[ii][0] for ii in range(0, len(X_train)) if y_train[ii] == 1]
     bumpy_bkg = [X_train[ii][1] for ii in range(0, len(X_train)) if y_train[ii] == 1]
 
-    #    training_data = {"fast":{"grade":grade_sig, "bumpiness":bumpy_sig}
-    #            , "slow":{"grade":grade_bkg, "bumpiness":bumpy_bkg}}
-
-
     grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii] == 0]
     bumpy_sig = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii] == 0]
     grade_bkg = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii] == 1]
     bumpy_bkg = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii] == 1]
 
     test_data = {"fast": {"grade": grade_sig, "bumpiness": bumpy_sig}
-        , "slow": {"grade": grade_bkg, "bumpiness": bumpy_bkg}}
+                 , "slow": {"grade": grade_bkg, "bumpiness": bumpy_bkg}}
 
     return X_train, y_train, X_test, y_test
 
