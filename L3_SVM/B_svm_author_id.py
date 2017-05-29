5e7d71eb42b3c1237359d6bbfc31cd9e14ca5b53
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-05-29 17:24:14 +0200

    fix: doc typo

--- L3_SVM/B_svm_author_id.py
+++ L3_SVM/B_svm_author_id.py
@@ -1,6 +1,6 @@
 """
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
-    Use a Naive Bayes Classifier to identify emails by their authors
+    Use a SVM Classifier to identify emails by their authors
     
     authors and labels:
     Sara has label 0
