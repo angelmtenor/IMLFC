28ac3558d853a6515c0e082e2d5669d68091115a
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-05-29 11:17:04 +0200

    feat: SVM on terrain data

--- L3_SVM/A_SVM_terrain_data.py
+++ L3_SVM/A_SVM_terrain_data.py
@@ -1,36 +1,23 @@
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+from time import time
 import sys
-from class_vis import prettyPicture
+sys.path.append("../tools/")
 from prep_terrain_data import makeTerrainData
-
-import matplotlib.pyplot as plt
-import copy
-import numpy as np
-import pylab as pl
-
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-
-########################## SVM #################################
-### we handle the import statement and SVC creation for you here
-from sklearn.svm import SVC
 clf = SVC(kernel="linear")
-
-
-#### now your job is to fit the classifier
-#### using the training features/labels, and to
-#### make a set of predictions on the test data
+t0 = time()
 
 clf.fit(features_train, labels_train)
+t1 = time()
 
-#### store your predictions in a list named pred
 pred = clf.predict(features_test)
+t2 = time()
 
+accuracy = accuracy_score(pred, labels_test)
 
-
-
-from sklearn.metrics import accuracy_score
-acc = accuracy_score(pred, labels_test)
-
-def submitAccuracy():
-    return acc
+print("accuracy: \t\t {:.6f}".format(accuracy))
+print("train time(s): \t {:.6f}".format(t1-t0))
+print("test time(s): \t {:.6f}".format(t2-t2))
