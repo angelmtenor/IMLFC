44b92730044e0b6264e003b5acd351aab4e622c4
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-05-30 11:12:38 +0200

    feat: Enron data quizzes

--- L6_Datasets/A_explore_enron_data.py
+++ L6_Datasets/A_explore_enron_data.py
@@ -12,15 +12,22 @@
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
     
 """
-# Original: https://github.com/udacity/ud120-projects/
-# Datasets:  https://github.com/udacity/ud120-projects/tree/master/final_project
+
+# Data: https://github.com/udacity/ud120-projects/tree/master/final_project
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "br"))
 
-print("people: {}".format(len(enron_data)))
-# print(list(people for people in enron_data))
+print("\n FIELDS:")
+print([fields for fields in enron_data["LAY KENNETH L"]])
+
+print("\n PEOPLE:")
+print([people for people in enron_data])
+
+
+
+print("\nn_people: {}".format(len(enron_data)))
 
 poi = [people for people in enron_data if enron_data[people]["poi"] == 1]
 print("POIs in data: {}".format(len(poi)))
@@ -29,7 +36,7 @@ with open("../final_project/poi_names.txt", 'r') as f:
     poi_names = f.readlines()[2:]
 print("Existing POIs: {}".format(len(poi_names)))
 
-# print(enron_data['PRENTICE JAMES'])
+#print(enron_data['PRENTICE JAMES'])
 stock_jp = enron_data['PRENTICE JAMES']['total_stock_value']
 print("Stock James Prentice {}".format(stock_jp))
 
@@ -38,3 +45,17 @@ print('mails from Wesley Colwell to POI: {}'.format(mails_to_poi_wc))
 
 stock_options_jp = enron_data['SKILLING JEFFREY K']['exercised_stock_options']
 print("Stock Options Jeffrey K Skilling {}".format(stock_options_jp))
+
+ceo = "LAY KENNETH L", "SKILLING JEFFREY K", "SKILLING JEFFREY K"
+ceo_pay = dict()
+for i in ceo:
+    ceo_pay[i] = enron_data[i]["total_payments"]
+max_ceo = max(ceo_pay, key=ceo_pay.get)
+print("CEO with max total payments: {}, with ${}".format(max_ceo, ceo_pay[max_ceo]))
+
+qs = [people for people in enron_data if float(enron_data[people]["salary"]) > 0]
+print("folks with quantified salary: {}".format(len(qs)))
+
+em = [i for i in enron_data if "@" in enron_data[i]["email_address"]]
+
+print("folks with email address: {}".format(len(em)))
