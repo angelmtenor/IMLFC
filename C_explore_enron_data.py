79856be56ddd7e0fe8c132245802d3e06afae957
Angel Martínez-Tenor <angelmtenor@gmail.com>
2017-06-01 20:41:06 +0200

    fix: classifiers' name

--- C_explore_enron_data.py
+++ C_explore_enron_data.py
@@ -17,7 +17,7 @@
 
 import pickle
 
-enron_data = pickle.load(open("final_project_dataset.pkl", "br"))
+enron_data = pickle.load(open("tools/final_project_dataset.pkl", "br"))
 
 print("\n FIELDS:")
 print([fields for fields in enron_data["LAY KENNETH L"]])
@@ -30,7 +30,7 @@ print("\nn_people: {}".format(n_people))
 poi = [people for people in enron_data if enron_data[people]["poi"] == 1]
 print("POIs in data: {}".format(len(poi)))
 
-with open("poi_names.txt", 'r') as f:
+with open("tools/poi_names.txt", 'r') as f:
     poi_names = f.readlines()[2:]
 print("Existing POIs: {}".format(len(poi_names)))
 
